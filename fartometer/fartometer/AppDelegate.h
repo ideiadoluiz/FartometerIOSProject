51dc1da8773d47c34c14e80fd4b859073a8605db
luizperes <luiz_peres10@hotmail.com>
2015-10-15 14:28:24 -0700

    Initial commit with the initial project

---
+++ fartometer/fartometer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fartometer
+//
+//  Created by Luiz Fernando Peres on 2015-10-15.
+//  Copyright © 2015 Ideia do Luiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ fartometer/fartometer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  fartometer
+//
+//  Created by Luiz Fernando Peres on 2015-10-15.
+//  Copyright © 2015 Ideia do Luiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
